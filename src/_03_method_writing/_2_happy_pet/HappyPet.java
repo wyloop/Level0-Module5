746ae8bb98c03239e7bf825fb1110b5122659203
wyloop <131419214+wyloop@users.noreply.github.com>
2025-05-03 19:59:01 +0000

    happy pet, calculator

--- src/_03_method_writing/_2_happy_pet/HappyPet.java
+++ src/_03_method_writing/_2_happy_pet/HappyPet.java
@@ -5,26 +5,86 @@ public class HappyPet {
 	
 	// 1. Add the following variable to the next line: static int happinessLevel = 0;
 	// this will be used to store the happiness of your pet
-	
+	static int happinessLevel = 0;
+	static String pet;
+	static int hunger;
 	public static void main(String[] args) {
 		// 2. Ask the user what kind of pet they want to buy, and store their answer in a variable
-
-		// 3. REPEAT steps 4 - 7 enough times to make your pet happy!
+		int petNum = JOptionPane.showOptionDialog(null, "Which pet would you like to adopt?", "Choose a Pet", 0, JOptionPane.INFORMATION_MESSAGE, null,
+				new String[] {"Dog", "Cat", "Hamster"}, null);
 		
+		if (petNum == 0) {
+			pet = "dog";
+			hunger = 2;
+		} else if (petNum == 1) {
+			pet = "cat";
+			hunger = 2;
+		} else {
+			pet = "hamster";
+			hunger = 1;
+		}
+		// 3. REPEAT steps 4 - 7 enough times to make your pet happy!
+		for (int i = 0; i < 6; i++) {
 			// 4. Use showOptionDialog to ask the user what they want to do to make their pet happy
 			//    (eg: cuddle, food, water, take a walk, groom, clean up poop).
 			//    Make sure to customize the title and question too.
-			int task = JOptionPane.showOptionDialog(null, "Question", "Title", 0, JOptionPane.INFORMATION_MESSAGE, null,
-					new String[] { "Button1", "Button2", "Button3" }, null);
+			int task = JOptionPane.showOptionDialog(null, "What would you like to do with your " + pet + "?", "Make Pet Happy", 0, JOptionPane.INFORMATION_MESSAGE, null,
+					new String[] { "Feed", "Pet", "Walk" }, null);
 
 			// 6. Use user input to call the appropriate method created in step 5 below.
-
+			if (task == 0) {
+				feed();
+			} else if (task == 1) {
+				pet();
+			} else {
+				walk();
+			}
 			// 7. If you determine the happiness level is large enough, tell the
 			//    user that he loves his pet and use break; to exit for loop.
+			if(happinessLevel >= 10) {
+				break;
+			}
+		}
 
+		JOptionPane.showMessageDialog(null, "Your pet is now very, very happy!", "You did it!", 1);
 	}
 
 	// 5. Create methods to handle each of your user selections.
 	//    Each method should create a pop-up with the pet's response (eg. cat might purr when pet), 
 	//    and INCREMENT the pet's happiness Level.
+
+	static void feed() {
+		if(hunger > 0) {
+			hunger -= 1;
+			happinessLevel += 2;
+			JOptionPane.showMessageDialog(null, "Your " + pet + " thanks you for feeding them. Current happiness score: " + happinessLevel, "Feed", 1);
+		} else {
+			JOptionPane.showMessageDialog(null, "Your " + pet + " is already full. Current happiness score: " + happinessLevel, "Feed", 1);
+		}
+		
+	}
+	static void pet() {
+		if (pet.equals("dog")) {
+			happinessLevel += 3;
+			JOptionPane.showMessageDialog(null, "Your dog enjoyed being pet. Current happiness score: " + happinessLevel, "Pet", 1);
+		} else if (pet.equals("cat")) {
+			happinessLevel += 2;
+			JOptionPane.showMessageDialog(null, "Your cat purrs as you pet it. Current happiness score: " + happinessLevel, "Pet", 1);
+		} else {
+			happinessLevel += 1;
+			JOptionPane.showMessageDialog(null, "Your hamster didn't really enjoy being pet. Current happiness score: " + happinessLevel, "Pet", 1);
+		}
+	}
+	static void walk() {
+		if (pet.equals("dog")) {
+			happinessLevel += 3;
+			JOptionPane.showMessageDialog(null, "Your dog enjoyed being going on a walk. Current happiness score: " + happinessLevel, "Walk", 1);
+		} else if (pet.equals("cat")) {
+			happinessLevel += 1;
+			JOptionPane.showMessageDialog(null, "Your cat didn't really enjoy going on a walk. Current happiness score: " + happinessLevel, "Walk", 1);
+		} else {
+			happinessLevel -= 2;
+			JOptionPane.showMessageDialog(null, "Your hamster didn't enjoy going on a walk. Why would you walk a hamster? Current happiness score: " + happinessLevel, "Walk", 1);
+		}
+	}
 }
--- src/_03_method_writing/_4_calculator/Calculator.java
+++ src/_03_method_writing/_4_calculator/Calculator.java
@@ -5,6 +5,7 @@ import javax.swing.JOptionPane;
 public class Calculator {
 
 	//GOAL: MAKE A CALCULATOR
+	static int result;
 	public static void main(String[] args) {
 		String input1 = JOptionPane.showInputDialog("Enter a number");
 		String input2 = JOptionPane.showInputDialog("Enter another number");
@@ -14,18 +15,39 @@ public class Calculator {
 				JOptionPane.INFORMATION_MESSAGE, null, new String[] { "divide", "multiply", "subtract", "add" }, null);
 
 		// 3) Call the correct method depending on what option the user chooses
-		
+		if(task == 0) {
+			divide(number1, number2);
+		} else if (task == 1) {
+			multiply(number1, number2);
+		} else if (task == 2) {
+			subtract(number1, number2);
+		} else {
+			add(number1, number2);
+		}
 		// 4) Call the result() method and put the answer in a pop-up
+		result();
 	}
 	
 	// 1) Make 4 static methods (add, subtract, multiply, divide)
 	//		They should each take 2 numbers as parameters
 	//		and return the answer.
-	 
-	
+	static void add(int one, int two) {
+		result = one + two;
+	}
+	static void subtract(int one, int two) {
+		result = one - two;
+	}
+	static void multiply(int one, int two) {
+		result = one * two;
+	}
+	static void divide(int one, int two) {
+		result = one / two;
+	}
+
 	// 2) Make a static method called " result" that takes a number
 	//		as a parameter and returns the number in a string
 	//		EX: "Your answer is " + number;
-
-	
+	static void result() {
+		JOptionPane.showMessageDialog(null, "Your answer is " + result, "Calculator", 1);
+	}	
 }
